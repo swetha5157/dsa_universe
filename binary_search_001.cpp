d909f5e8387976a6226626947d285288a559b0a9
Swetha <swethasuresh799@gmail.com>
2026-06-21 23:12:39 +0530

    peak element more understandable code

--- binary_search_001.cpp
+++ binary_search_001.cpp
@@ -148,18 +148,22 @@ int searchWithDuplicates(vector<int>& nums, int target) {
 
 // find the peak element in a mountain array, tc: O(log n), sc: O(1)
 // return index of the peak element
-int peakElement(vector<int> &arr) {
+ int peakElement(vector<int> &arr) {
         // code here
         int n=arr.size();
-        int l=0,r=n-1;
-        while(l<r){
+        if(n==1) return 0;//only 1 element
+        if(arr[0]>arr[1]) return 0; // first element is peak
+        if(arr[n-1]>arr[n-2]) return n-1;// last element is peak
+    
+        int l=1,r=n-2;
+        while(l<=r){
             int mid=(l+r)/2;
-            if(arr[mid]>arr[mid+1]){
-                // we are in the decreasing part of the array, so the peak element is in the left half (including mid)
-                r=mid;
-            }else l=mid+1;
+            if(arr[mid]>arr[mid+1] && arr[mid-1]<arr[mid])// eg: 1 3 5 4 2, mid=5, arr[mid]>arr[mid+1] && arr[mid-1]<arr[mid]
+            {  return mid;}
+            else if(arr[mid]>arr[mid-1])  l=mid+1;
+            else r=mid-1;
         }
-        return l;
+        return -1;
     }
 
 // search in a bitonic array
